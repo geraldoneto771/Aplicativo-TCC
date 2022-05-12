8a00bf5bbbdd5b1fec4daa1f1b63a73a417a5cf6
geraldoneto771 <geraldopireslacerda771@gmail.com>
2022-05-12 18:56:48 -0300

    Adicionado botão de voltar para cena anterior e porcentagem na cena

--- Assets/Scripts/ChangeImageScenes.cs
+++ Assets/Scripts/ChangeImageScenes.cs
@@ -6,42 +6,58 @@ using System;
 using UnityEngine.UI;
 using DG.Tweening;
 using System.Linq;
+using TMPro;
+
 
 public class ChangeImageScenes : MonoBehaviour
 {
 
-    
     public AudioClip lofi;
     public AudioClip introducao;
     //Vetor de imagens contendo as cenas da aventura
     public Image cenasDoJogo;
     public Sprite[] sprites;
-
     int index;
+    public TMP_Text textCenas;
+    private float porcentagemCenas;
+
 
     void Start()
     {
-        
-        //index = 0;
-        //spriteRender = GetComponent<SpriteRenderer>();
-        //spriteRender.sprite = sprites[0];
-
-
         //Renderizando os sprites no vetor
         sprites = Resources.LoadAll("Cenas Introdução", typeof(Sprite)).Cast<Sprite>().ToArray();
         index = 0;
         cenasDoJogo = this.GetComponent<Image>();
         cenasDoJogo.sprite = sprites[index];
+        porcentagemCenas = 100f/26f;
+        textCenas.GetComponent<TMP_Text>().text = string.Format("{0:0.00}", porcentagemCenas) + "%";
         AudioController.instance.PlayMusic(introducao);
+
     }
 
+    //Função para prosseguir para cena posterior
     public void NextChangeImage()
     {
         index++;
-
+        porcentagemCenas = porcentagemCenas + (100f/26f);
+        
+        textCenas.GetComponent<TMP_Text>().text = string.Format("{0:0.00}", porcentagemCenas) + "%";
         cenasDoJogo.sprite = sprites[index];
 
     }
+    //Função para retornar para cena anterior
+    public void PreviousChangeImage()
+    {
+       if(index >= 1)
+        {
+            index--;
+            porcentagemCenas = porcentagemCenas - (100f / 26f);
+            textCenas.GetComponent<TMP_Text>().text = string.Format("{0:0.00}", porcentagemCenas) + "%";
+            cenasDoJogo.sprite = sprites[index];
+        }
+
+    }
+    //Função para retornar para a tela de seleção de aventuras
     public void LoadScene(string name)
     {
         if (index >= 26)
@@ -50,7 +66,6 @@ public class ChangeImageScenes : MonoBehaviour
             SceneLoader.Instance.LoadSceneAsync(name);
             AudioController.instance.PlayMusic(lofi);
         }
-
-        
+     
     }
 }
