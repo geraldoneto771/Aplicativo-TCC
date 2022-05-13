f2d65b2db06a0f3289bf882338b3d28f081b3b36
geraldoneto771 <geraldopireslacerda771@gmail.com>
2022-05-12 21:30:40 -0300

    adicionado msg de erro ao quebrar cofre sem nada

--- Assets/Scripts/Cofrinho.cs
+++ Assets/Scripts/Cofrinho.cs
@@ -102,6 +102,46 @@ public class Cofrinho : MonoBehaviour{
     public void quebrarCofre()
     {
         bool isActive = PanelTemCertezaCofre.activeSelf;
+
+        try{
+            if (jogador.saldoCofre <= 0)
+            {
+                Debug.Log("Antes de tentar quebrar o cofrinho, que tal colocar dinheiro dentro dele?");
+                textMensagemErro.GetComponent<Text>().text = "Antes de tentar quebrar o cofrinho, que tal colocar dinheiro dentro dele?";
+                PanelTemCertezaCofre.SetActive(!isActive);
+            }
+            else if (jogador.saldoCofre > 0)
+            {
+                jogador.quebraCofre();
+                textTotalCofre.GetComponent<Text>().text = "R$" + jogador.saldoCofre.ToString();
+
+
+                if (PanelTemCertezaCofre != null)
+                {
+                    jogador.quebraCofre();
+                    PanelTemCertezaCofre.SetActive(!isActive);
+                    inputMoney.text = "";
+
+                    audioEffect.clip = cofreAudioEffects[1];
+                    audioEffect.Play();
+
+                }
+                else
+                {
+                    PanelTemCertezaCofre.SetActive(isActive);
+
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Que tal colocar dinheiro dentro do cofrinho primeiro?" + e);
+            textMensagemErro.GetComponent<Text>().text = "Que tal colocar dinheiro dentro do cofrinho primeiro?";
+            PanelTemCertezaCofre.SetActive(!isActive);
+        }
+
+        /*
         valor = 0;
         textTotalCofre.GetComponent<Text>().text = "R$" + valor.ToString();
         if (PanelTemCertezaCofre != null)
@@ -119,6 +159,8 @@ public class Cofrinho : MonoBehaviour{
             PanelTemCertezaCofre.SetActive(isActive);
 
         }
+
+        */
 
     }
 
